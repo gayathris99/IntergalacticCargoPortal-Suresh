eb827d7d096ecd7424e95c91301aed86ba13cd0f
Gayathri S <gayathris@Gayathris-MacBook-Pro.local>
2026-05-22 17:38:19 +0530

    Environment variables update for hosting

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 24 * 60 * 60 * 1000
         })
         res.status(200).json({ role: user.role })
@@ -99,8 +99,9 @@ router.post('/logout', async (req, res) => {
     res.clearCookie('token', {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict'
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     })
+    
     res.status(200).json({
         message: 'User logged out successfully'
     })
