ff7dd42da952a66d473d0d2cd0abc7291f5c474b
Gayathri S <gayathris@Gayathris-MacBook-Pro.local>
2026-05-22 12:27:59 +0530

    Task 1 - Added logout, changed port number for local env as it was causing issues

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -94,4 +94,16 @@ router.post('/signup', async (req, res) => {
 
 })
 
+// logout
+router.post('/logout', async (req, res) => {
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict'
+    })
+    res.status(200).json({
+        message: 'User logged out successfully'
+    })
+})
+
 module.exports = router
